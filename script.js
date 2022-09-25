2cbcc34c81d2139af05dfae00224c28c7454d419
Immanuel Pettigrew <pettigrew.i@gmail.com>
2022-09-25 19:31:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var rainbow = Math.floor(Math.random() * 9);
+var bomb = Math.floor(Math.random() * 9);
+console.log(rainbow);
+console.log(bomb);
+
+const treasure = (id) => {
+  console.log(id);
+  if (rainbow === id) {
+    document.getElementById(id).innerHTML = '&#x1f308';
+    alert("You've found te rainbow ");
+    console.log("You've found the rainbow.");
+  } else if (bomb === id) {
+    document.getElementById(id).innerHTML = '&#x2620';
+    alert('-Bomb');
+    console.log('Bomb');
+  } else if (rainbow === bomb) {
+    alert('Try again, Same...');
+  } else {
+    document.getElementById(id).innerHTML = '&#x1f332';
+    alert('Keep clicking.');
+    console.log('Keep clicking.');
+  }
+};
